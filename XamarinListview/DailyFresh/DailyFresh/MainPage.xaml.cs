81b22849fc174b0fb98ee072f864ff2a9733deae
Aruljebaraj <34904782+Aruljebaraj@users.noreply.github.com>
2018-03-15 11:58:53 +0530

    XamarinListview
    XamarinListview

---
+++ XamarinListview/DailyFresh/DailyFresh/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DailyFresh
+{
+	public partial class MainPage : ContentPage
+	{
+
+        public int Count = 0;
+        public short Counter = 0;
+        public int SlidePosition = 0;
+        string heightList;
+        int heightRowsList = 90;
+        private const string Url = "http://rallycoding.herokuapp.com/api/music_albums";
+        // This handles the Web data request
+        private HttpClient _client = new HttpClient();
+        public MainPage()
+		{
+			InitializeComponent();
+            OnGetList();
+        }
+        protected async void OnGetList()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                try
+                {
+                    //Activity indicator visibility on
+                  //  activity_indicator.IsRunning = true;
+                    //Getting JSON data from the Web
+                    var content = await _client.GetStringAsync(Url);
+                    //We deserialize the JSON data from this line
+                    var tr = JsonConvert.DeserializeObject<List<Model>>(content);
+                    //After deserializing , we store our data in the List called ObservableCollection
+                    ObservableCollection<Model> trends = new ObservableCollection<Model>(tr);
+
+                    //Then finally we attach the List to the ListView. Seems Simple :)
+                    myList.ItemsSource = trends;
+
+
+                    //We check the number of Items in the Observable Collection
+                    int i = trends.Count;
+                    if (i > 0)
+                    {
+                        //If they are greater than Zero we stop the activity indicator
+                    //    activity_indicator.IsRunning = false;
+                    }
+                    heightList = trends.Count.ToString();
+                    PendingCount.Text = heightList;
+                    //Here we Wrap  the size of the ListView according to the number of Items which have been retrieved 
+                    i = (trends.Count * heightRowsList);
+                  //  activity_indicator.HeightRequest = i;
+
+                }
+                catch (Exception ey)
+                {
+                    Debug.WriteLine("" + ey);
+                }
+
+            }
+
+        }
+
+    }
+}
+
---
+++ XamarinListview/DailyFresh/DailyFresh/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyFresh
+{
+    class Model
+    {
+        public string image { get; set; }
+        public string title { get; set; }
+        public string artist { get; set; }
+        public string thumbnail_image { get; set; }
+        public string url { get; set; }
+    }
+}
